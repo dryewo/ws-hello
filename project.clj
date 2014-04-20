80c4833710eec83c21c6713aee82b7fa89d06481
Dmitry Balakhonskiy <dnomaid@gmail.com>
2014-04-20 13:20:58 +0400

    Setup server

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [http-kit "2.1.18"]
+                 [metosin/compojure-api "0.10.4"]
+                 [javax.servlet/servlet-api "2.5"]]
   :main ^:skip-aot ws-hello.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ws_hello/core.clj
+++ src/ws_hello/core.clj
@@ -1,7 +1,43 @@
 (ns ws-hello.core
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server]
+            [ring.util.http-response :refer :all]
+            [compojure.api.sweet :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn add-reader [])
+
+(defn add-writer [])
+
+(defn wrap-cache-control [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Cache-control"] "no-cache"))))
+
+(defn wrap-log [handler]
+  (fn [request]
+    (let [req-id (apply str (take 4 (repeatedly #(rand-nth "ABCDEF0123456789"))))]
+      (println req-id
+                (->> (:request-method request) name clojure.string/upper-case)
+                (:uri request)
+                (:params request))
+      (let [response (handler request)]
+        (println req-id
+                  (:status response)
+                  (:body response))
+        response))))
+
+(defapi app
+  (with-middleware [wrap-log wrap-cache-control]
+    (GET* "/"      [] (resource-response "index.html" {:root "public"}))
+    (GET* "/read"  [] add-reader)
+    (GET* "/write" [] add-writer)
+    compojure.api.middleware/public-resource-routes
+    (compojure.route/not-found "<h1>NO.</h1>")))
+
+(defn -main [& args]
+  (let [port (or (first args) 4040)]
+    (println "Starting on port" port)
+    (org.httpkit.server/run-server #'app {:port (bigdec port)})))
+
+;(def stop-server (-main))
+;(stop-server)
